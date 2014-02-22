2245d09536891b67e6ec66502b17f956a4adaef4
volkan.ceylan <volkanceylan@gmail.com>
2014-02-22 23:06:56 +0200

    remove unused references

--- Serenity.Core/Data/Sql/SqlQuery/SqlQuery_Entity.cs
+++ Serenity.Core/Data/Sql/SqlQuery/SqlQuery_Entity.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Data;
 
     public partial class SqlQuery : QueryWithParams, IFilterableQuery, IGetExpressionByName
     {
